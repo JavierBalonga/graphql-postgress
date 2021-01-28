494d877cc5aa4f3cf6d4261b946f5f99e3ffadb5
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2021-01-28 09:50:53 -0300

    fix(src/server.js)cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import graphql from "./graphql";
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => res.redirect("/graphql"));
 
